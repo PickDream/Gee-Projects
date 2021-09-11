7a0e45259783e192d92e5079aa7b535513d842c9
Mason.Lu <lumaoxin@foxmail.com>
2021-09-11 10:08:32 +0800

    geecache单机版功能完成

---
+++ geecache/geecache.go
@@ -0,0 +1,82 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+//回调获取值的接口
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// 定义一个函数类型，方便使用者在调用的时候能直接传入函数作为参数
+type GetterFunc func(key string) ([]byte, error)
+
+//函数类型实现接口方法，被成为接口型函数，这样的话 Getter 方法的实现既可以是以恶搞函数类型
+//也可以是一个实现了 Getter 接口的结构体类型
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.Unlock()
+	return groups[name]
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
