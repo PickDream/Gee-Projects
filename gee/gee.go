a945fdb47c61f243318c4dfd7381f2631bfc2793
Mason.Lu <lumaoxin@aliyun.com>
2021-09-14 22:51:39 +0800

    Gee day01

---
+++ gee/gee.go
@@ -0,0 +1,13 @@
+package gee
+
+import "net/http"
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
