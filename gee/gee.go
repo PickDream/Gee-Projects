f4549fb7a564e4174420918e315cb2a2c93f30f9
Mason.Lu <lumaoxin@aliyun.com>
2021-09-15 21:35:13 +0800

    完成Gee的处理器注册功能

--- gee/gee.go
+++ gee/gee.go
@@ -1,8 +1,18 @@
 package gee
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+var (
+	GET    = "GET"
+	POST   = "POST"
+	PUT    = "PUT"
+	DELETE = "DELETE"
+)
 
 type Engine struct {
 	router map[string]HandlerFunc
@@ -10,4 +20,38 @@ type Engine struct {
 
 func New() *Engine {
 	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRouter(GET, pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRouter(POST, pattern, handler)
+}
+
+func (engine *Engine) PUT(pattern string, handler HandlerFunc) {
+	engine.addRouter(PUT, pattern, handler)
+}
+
+func (engine *Engine) DELETE(pattern string, handler HandlerFunc) {
+	engine.addRouter(DELETE, pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
 }
