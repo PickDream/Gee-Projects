c101521b9de5ae33db0094e23c3981097c7338d5
Mason.Lu <lumaoxin@aliyun.com>
2021-09-17 22:36:02 +0800

    增加route方法

--- gee/router.go
+++ gee/router.go
@@ -1,21 +1,44 @@
 package gee
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
 	handlers map[string]HandlerFunc
+	roots    map[string]*node
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{
+		handlers: make(map[string]HandlerFunc),
+		roots:    make(map[string]*node),
+	}
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
-	log.Printf("Route %4s - %s", method, pattern)
+	parts := parsePattern(pattern)
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 
 }
@@ -28,4 +51,31 @@ func (r *router) handle(c *Context) {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s \n", c.Path)
 	}
 
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchPaths := parsePattern(path)
+
+	params := make(map[string]string)
+
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchPaths, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = strings.Join(searchPaths[index:], "/")
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchPaths[index:], "/")
+			}
+		}
+		return n, params
+	}
+	return nil, nil
 }
