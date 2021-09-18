ac453e7d86f8386b53fdf72a59e71bff7445499a
Mason.Lu <lumaoxin@aliyun.com>
2021-09-18 21:58:54 +0800

    完善router，增加尝试用例

--- gee/router.go
+++ gee/router.go
@@ -44,11 +44,13 @@ func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 }
 
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
 	} else {
-		c.String(http.StatusNotFound, "404 NOT FOUND: %s \n", c.Path)
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
 
 }
@@ -69,7 +71,7 @@ func (r *router) getRoute(method string, path string) (*node, map[string]string)
 		parts := parsePattern(n.pattern)
 		for index, part := range parts {
 			if part[0] == ':' {
-				params[part[1:]] = strings.Join(searchPaths[index:], "/")
+				params[part[1:]] = searchPaths[index]
 			}
 			if part[0] == '*' && len(part) > 1 {
 				params[part[1:]] = strings.Join(searchPaths[index:], "/")
